037e7a4021a8b449f55a944a97e9bcd63d051f05
s-yoshihara <s-yoshihara@saitama-tec.co.jp>
2018-10-02 13:12:18 +0000

    test add

--- src/gacha.ts
+++ src/gacha.ts
@@ -3,14 +3,33 @@ interface GachaInterface{
   public Cast();
 }
 
-
 namespace SaitamaTec.SimpleGacha{
   export default abstract class Gacha implements GachaInterface{
 
+    private $name:string;
+    public get Name():string{
+
+      return $name;
+    }
+
+
+    public constructor(
+      $name:string,
+      $gacha:GachaTable
+    ){
+      this.$name=$name;
+
+    }
+
     public Cast():GachaItem{
+      
+
+       
       return null;
     }
   }
+
+
   export class GachaItem{
     public Name:string;
     public Id:number;
@@ -19,10 +38,33 @@ namespace SaitamaTec.SimpleGacha{
     }
   }
 
+  export class GachaTable{
+
+    public Cast():GachaItem{
+      
+    }
+
+    public constructor(
+      $list:Array<GachaRecord> =[]
+      ){
+    }
+  }
+
+  export class GachaRecord{
+    public quantity:number;
+    public Label:string;
+    public SubGacha:GachaTable=null;
+    
+    
+    public constructor(
+      $q:number,
+      $label:string,
+    ){
+      
+    }
+  }
+
 }
 
-export class SimpleGacha extends Gacha{
-  public Cast():{
-    console.log("GachaOk");
-  }
-}
+
+export default SaitamaTec.SimpleGacha.Gacha;
